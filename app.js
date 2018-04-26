f682bd7d99531791ea1c7443efb39e09d407e7d9
Peep Noorkoiv <peepnoor@gmail.com>
2018-04-26 16:23:56 +0000

    Added estonian and english page to site

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ var express = require("express"),
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+// Landing page
 app.get("/", function(req, res){
     res.render("landing");
+});
+
+// Eesti keelne portfolio
+app.get("/ET", function(req, res){
+    res.render("index_ET");
+});
+
+// English portfolio
+app.get("/EN", function(req, res){
+    res.render("index_EN");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
